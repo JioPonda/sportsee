38cf7a5ea03f261e18af8d59ff8b15b6fc02c6a9
Jioponda <crazyponda@outlook.fr>
2023-01-17 17:02:48 +0100

    Ajout d'assets + création de la NavbarTop + mokage des Datas

---
+++ src/Components/Layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavbarTop from "./NavbarTop";
+
+const Layout = () => {
+  return (
+    <>
+      <NavbarTop />
+    </>
+  );
+};
+
+export default Layout;
--- src/Components/NavbarTop.jsx
+++ src/Components/NavbarTop.jsx
@@ -1,2 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import Logo from "../assets/logo_sportsee.png";
+
+const NavbarTop = () => {
+  return (
+    <header>
+      <img src={Logo} alt="logo SportSee" className="logo" />
+      <nav>
+        <ul>
+          <li className="acceuil_navbar">Acceuil</li>
+          <li className="profil_navbar">Profil</li>
+          <li className="setup_navbar">Réglage</li>
+          <li className="community_navbar">Communauté</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default NavbarTop;
