fccc0b9adca647c0820882fd4ab1870531037c79
Jioponda <crazyponda@outlook.fr>
2023-01-27 17:24:24 +0100

    test avec useState

--- src/Components/Objectif.jsx
+++ src/Components/Objectif.jsx
@@ -1,13 +1,34 @@
-import React from "react";
-import Objectifchart from "./Objectifchart";
+import { React, useState, useEffect } from "react";
+import { useParams } from "react-router";
+import { LineChart, Line, ResponsiveContainer } from "recharts";
 import Ofilter from "../assets/objectif_filter.png";
+import Objectifuser from "../data/user_average_sessions.json";
 
-const Objectif = () => {
+const Objectif = (data) => {
+  let idUser = useParams();
+  const [objectifArray, setObjectifArray] = useState([]);
+
+  Objectifuser.forEach((user) => {
+    if (Number.parseInt(idUser.id === user.userId))
+      objectifArray.push(user.sessions);
+    console.log(objectifArray);
+    return objectifArray;
+  });
+
   return (
     <div className="objectif">
       <img src={Ofilter} alt="" className="objectif_filter" />
       <p className="duration">Durée moyenne des sessions</p>
-      <Objectifchart />
+      <ResponsiveContainer width="100%" height="80%">
+        <LineChart width={300} height={100} data={objectifArray}>
+          <Line
+            type="monotone"
+            dataKey="sesisonsLength"
+            stroke="#FFF"
+            strokeWidth={4}
+          />
+        </LineChart>
+      </ResponsiveContainer>
       <p className="days">L M M J V S D</p>
     </div>
   );
